f62bfc1faebac4fc2831cbd082d722df8169a150
Lenuta Flama <lena.flama@gmail.com>
2024-03-13 14:52:47 +0200

    Solve a minor error regarding an unused <NavLink> tag

--- src/components/features/header/home/home.js
+++ src/components/features/header/home/home.js
@@ -1,14 +1,12 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { updateDefaultSubreddit } from "../../../Cards/cardsSlice";
-import { NavLink } from "react-router-dom";
 
 export default function Home() {
   const dispatch = useDispatch();
   const handleChangeSubredditToHome = () => {
     dispatch(updateDefaultSubreddit("r/pics/"));
   };
-
   useEffect(() => {
     handleChangeSubredditToHome();
   });
